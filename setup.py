8e0c02b9e7ed14785428796c06d64e474c126d95
mdisi2 <157818999+mdisi2@users.noreply.github.com>
2025-03-11 22:48:42 -0500

    Update setup.py
    put it back to get_git_version() because it didnt solve issue

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ long_description = read('README.md', 'CONTRIBUTING.md')
 
 setup(
     name='pyrk',
-    version='unknown',
+    version=get_git_version(),
     url='http://github.com/katyhuff/pyrk/',
     license='BSD 3-Clause License',
     author='Kathryn D. Huff',
