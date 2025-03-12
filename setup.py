24e57ed13371da405f0042a8c378aed03be19f0f
mdisi2 <157818999+mdisi2@users.noreply.github.com>
2025-03-12 01:06:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ long_description = read('README.md', 'CONTRIBUTING.md')
 
 setup(
     name='pyrk',
-    version='3.12',
+    version='0.3.12',
     url='http://github.com/katyhuff/pyrk/',
     license='BSD 3-Clause License',
     author='Kathryn D. Huff',
