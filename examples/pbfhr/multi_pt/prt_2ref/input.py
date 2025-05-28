e2bbe977417fdae3c06bd6362b9e32ad462f7c37
mdisi2 <157818999+mdisi2@users.noreply.github.com>
2025-05-28 10:43:43 -0500

    Update input.py
    added pyrk prefix to reactivity_insertion

--- examples/pbfhr/multi_pt/prt_2ref/input.py
+++ examples/pbfhr/multi_pt/prt_2ref/input.py
@@ -103,7 +103,7 @@ ref_rho = [0.084349, 0.168983]
 feedback = True
 
 # External Reactivity
-from reactivity_insertion import RampReactivityInsertion
+from pyrk.reactivity_insertion import RampReactivityInsertion
 # from reactivity_insertion import StepReactivityInsertion
 # rho_ext = StepReactivityInsertion(timer=ti,
 #                                  t_step=t_feedback + 10.0*units.seconds,
